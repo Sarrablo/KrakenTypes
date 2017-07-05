242e3630015535d1b7f974c9d838556fc649829b
sarrablo <gsarrablo@gmail.com>
2017-07-05 23:37:41 +0200

    first commit

---
+++ krakentypes.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+
+class Time():
+
+    def __init__(self, dictio):
+        d_named = namedtuple("Time", dictio.keys())(*dictio.values())
+        result = namedtuple("Result", d_named.result.keys())(*d_named.result.values())
+        self.unix_time = result.unixtime
+        self.date_time = result.rfc1123
+
