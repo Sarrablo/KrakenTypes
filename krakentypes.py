d2d5cc2c2621a9b5534e1defda91d305462b760c
sarrablo <gsarrablo@gmail.com>
2017-07-06 14:59:24 +0200

    Added Assets and Assets Pair

--- krakentypes.py
+++ krakentypes.py
@@ -7,3 +7,15 @@ class Time():
         self.unix_time = result.unixtime
         self.date_time = result.rfc1123
 
+class Assets():
+    def __init__(self, dictio):
+        d_named = namedtuple("Time", dictio.keys())(*dictio.values())
+        self.assets = namedtuple("Assets", d_named.result.keys())(*d_named.result.values())
+
+class AssetPairs():
+    def __init__(self, dictio):
+        for k,v in dictio.items():
+            if isinstance(v,dict):
+                self.__dict__[k] = AssetPairs(v)
+            else:
+                self.__dict__[k] = v
